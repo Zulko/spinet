1237e6508554b15cc3d315c99e7622e6564db3f3
Zulko <valentin.zulkower@gmail.com>
2018-06-25 21:14:02 +0100

    first satifactory version. UK keyboard only

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import prototype from '@/components/prototype'
+import spinet from '@/components/spinet'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'prototype',
-      component: prototype
+      name: 'spinet',
+      component: spinet
     }
   ]
 })
